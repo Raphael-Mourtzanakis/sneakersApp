f67977364c9a961903e08543ecf7de2bca5096d5
Raphael MOURTZANAKIS <rmour@lmdsio.com>
2025-09-17 17:29:11 +0200

    Version 1

---
+++ src/Components/Banner.jsx
@@ -0,0 +1,22 @@
+import logo from '../assets/logosneaker.jpg';
+import '../styles/Banner.css';
+
+function Banner() {
+  return (
+    <header className="banner">
+      {/* Affichage en JSX du Logo de la boutique en ligne */}
+      <img src={logo} className="banner-logo" alt="Logo Sneakers" />
+
+      {/* Section contenant le titre et la description */}
+      <div className="banner-content">
+        {/* Titre principal de l'application */}
+        <h1>Sneakers Store</h1>
+
+        {/* Slogan attractif pour les visiteurs */}
+        <p>Trouvez les sneakers de vos rêves</p>
+      </div>
+    </header>
+  );
+}
+
+export default Banner;
