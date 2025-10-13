fce25c17562d53201d0aec4be91f144204a8de0b
Raphael MOURTZANAKIS <rmour@lmdsio.com>
2025-10-13 12:41:16 +0200

    Version 9.2

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,8 +1,14 @@
-import '../styles/App.css';
+import {useState} from "react";
 import Banner from "./Banner";
 import ShoppingList from "./ShoppingList";
+import Cart from ".\Cart";
+import "../styles/App.css";
 
 function App() {
+  const [cart, setCart] = useState([]);
+  /*const addToCart = (sneaker) => {
+
+  }*/
   return (
     <>
       < Banner />
