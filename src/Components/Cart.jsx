0ae1104043e1e065d12a06b01d6b1cf02d99e106
Raphael MOURTZANAKIS <rmour@lmdsio.com>
2025-11-03 12:19:26 +0100

    Version 13

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -1,4 +1,8 @@
 function Cart({cartItems}) {
+    const totalItems = cartItems.reduce((sum, item) =>
+        sum + item.quantity,0);
+    const totalPrice = cartItems.reduce((sum, item) =>
+        sum + (item.prix) * (item.quantity),0);
     return (
         <span>Blabla</span>
     )
